694f2f5acfe36d1e96ee39c9f69a9198d710096c
Vahn Gomes <me@vahngomes.dev>
2024-04-13 14:08:04 -0400

    Add redirect to pkg.go.dev if go-get is not provided

--- handler.go
+++ handler.go
@@ -17,6 +17,13 @@ func NewRouter(cfg *Config) *mux.Router {
 		path, repo := pkg.Path, pkg.Repo
 		router.HandleFunc("/"+path, func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Content-Type", "text/html")
+
+			// If go-get is not provided, redirect to the pgk.go.dev page
+			if r.URL.Query().Get("go-get") != "1" {
+				http.Redirect(w, r, "https://pkg.go.dev/"+cfg.Domain+"/"+path, http.StatusFound)
+				return
+			}
+
 			fmt.Fprintf(w, `<meta name="go-import" content="%s git %s">`, cfg.Domain+"/"+path, repo)
 		}).Methods("GET")
 	}
