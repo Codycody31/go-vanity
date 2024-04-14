b04e1a08b5cdd5bae29438c83a6b23bbf6fb1dab
Vahn Gomes <me@vahngomes.dev>
2024-04-14 17:45:44 -0400

    Add VANITY_IN_CONTAINER environment variable and update CLI usage

--- main.go
+++ main.go
@@ -8,12 +8,15 @@ import (
 	"github.com/urfave/cli/v2" // Import the CLI package
 	"go.codycody31.dev/vanity/config"
 	"go.codycody31.dev/vanity/server"
+	"go.codycody31.dev/vanity/version"
 )
 
 func main() {
 	app := &cli.App{
-		Name:  "VanityServer",
-		Usage: "A server for vanity URLs using the specified configuration file",
+		Name:                 "vanity",
+		Usage:                "A server for vanity URLs using the specified configuration file",
+		EnableBashCompletion: true,
+		Version:              version.String(),
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:    "config",
@@ -27,10 +30,21 @@ func main() {
 				Value:   "",
 				Usage:   "URL to fetch the config file from",
 			},
+			&cli.BoolFlag{
+				Name:    "in-container",
+				EnvVars: []string{"VANITY_IN_CONTAINER"},
+				Value:   false,
+				Usage:   "Set to true if running in a container",
+			},
 		},
 		Action: func(c *cli.Context) error {
 			configPath := c.String("config")
 			configURL := c.String("config-url")
+
+			if configPath == "" && c.Bool("in-container") {
+				configPath = "/etc/vanity/config.yaml"
+			}
+
 			cfg, err := config.LoadConfig(configPath, configURL)
 			if err != nil {
 				log.Fatalf("Failed to load config: %v", err)
