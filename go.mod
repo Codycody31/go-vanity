37122ee8c284f70da00f97add9735585709c796e
Insidious Fiddler <me@codycody31.dev>
2024-04-17 10:16:23 -0400

    Switch loggers & use template for html (#17)
    * Update .gitignore and server/handler.go, build-dev and build commands in Makefile, go.mod, cmd/vanity/flags.go, shared/logger/terminal.go, and cmd/vanity/main.go
    
    * Update server handler to use templates for root packages page
    
    * Fix error handling in LoadConfig and NewRouter functions
    
    * Update config struct to include "PoweredBy" field
    
    * Restyle Switch loggers & use template for html (#18)
    
    * Restyled by jq
    
    * Restyled by prettier-json
    
    * Restyled by whitespace
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    ---------
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- go.mod
+++ go.mod
@@ -10,13 +10,20 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
+	github.com/6543/logfile-open v1.2.1
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/rs/zerolog v1.32.0
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/stretchr/testify v1.9.0
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/term v0.19.0
 )
--- server/handler.go
+++ server/handler.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"go.codycody31.dev/vanity/config"
+	"go.codycody31.dev/vanity/templates"
 
 	"github.com/gorilla/mux"
 )
@@ -37,61 +38,15 @@ func NewRouter(cfg *config.Config) *mux.Router {
 	if !cfg.DisableRootPackagesPage {
 		// If index is requested, show the list of packages with links to both package details and repository
 		router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "text/html")
-			_, err := fmt.Fprintf(w, `<!DOCTYPE html>
-<html>
-<head>
-	<title>Packages</title>
-	<style>
-		body {
-			font-family: Arial, sans-serif;
-			margin: 40px;
-			background-color: #f4f4f9;
-			color: #333;
-		}
-		h1 {
-			color: #5a5a5a;
-		}
-		ul {
-			list-style-type: none;
-			padding: 0;
-		}
-		li {
-			margin: 10px 0;
-			padding: 10px;
-			background-color: #fff;
-			border: 1px solid #ddd;
-		}
-		a {
-			text-decoration: none;
-			color: #0066cc;
-		}
-		a:hover {
-			text-decoration: underline;
-		}
-		.repo-link {
-			color: #007700; /* Dark green for visibility */
-		}
-	</style>
-</head>
-<body>
-	<h1>Available Packages</h1>
-	<ul>
-	`)
+			err := templates.Templates.ExecuteTemplate(w, "index.html", map[string]interface{}{
+				"Packages":  cfg.Packages,
+				"PoweredBy": cfg.PoweredBy,
+			})
+
 			if err != nil {
-				return
+				http.Error(w, err.Error(), http.StatusInternalServerError)
 			}
-			for _, pkg := range cfg.Packages {
-				_, err := fmt.Fprintf(w, `<li><a href="/%s">%s</a> (<a href="%s" class="repo-link">Repo</a>)</li>`, pkg.Path, pkg.Path, pkg.Repo)
-				if err != nil {
-					return
-				}
-			}
-			_, err = fmt.Fprintf(w, "</ul></body></html>")
-			if err != nil {
-				return
-			}
-		}).Methods("GET")
+		})
 	}
 
 	return router
