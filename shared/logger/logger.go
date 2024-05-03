1579b7572ba9aabef94e951dbd1ef146055d096e
Vahn Gomes <me@vahngomes.dev>
2024-05-03 09:56:08 -0400

    chore: Rename WOODPECKER to VANITY for env vars

--- shared/logger/logger.go
+++ shared/logger/logger.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Modifications: Renamed WOODPECKER to VANITY for env vars
+
 package logger
 
 import (
